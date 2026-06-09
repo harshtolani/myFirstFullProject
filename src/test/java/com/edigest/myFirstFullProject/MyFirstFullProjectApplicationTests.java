d880ad019b295e9f1176a498e3f3168ff032da0a
harshtolani <138798346+harshtolani@users.noreply.github.com>
2026-06-09 23:27:54 +0530

    video-7

---
+++ src/test/java/com/edigest/myFirstFullProject/MyFirstFullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edigest.myFirstFullProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstFullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
